574bc72acb0973c7e045d397f055f5f114697d07
marlus <marlusrp@gmail.com>
2026-08-03 01:20:21 -0300

    correct categories for cycle 2

---
+++ backend/Migrations/20260803040739_Cycle2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineCall.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cycle2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
